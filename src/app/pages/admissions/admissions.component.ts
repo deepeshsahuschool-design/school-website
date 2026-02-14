95f7c03df8b9d64a5421f276dfca6d4a1ffdaacf
Deepesh Kumar Sahu <deepesh@cdot.in>
2026-02-15 00:09:58 +0530

    updated the site

--- src/app/pages/admissions/admissions.component.ts
+++ src/app/pages/admissions/admissions.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-admissions',
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './admissions.component.html',
   styleUrl: './admissions.component.scss'
 })
 export class AdmissionsComponent {
+  constructor(private http: HttpClient) {}
 
+  submitForm(form: any) {
+
+    if (form.invalid) {
+      alert("Please fill all required fields");
+      return;
+    }
+
+    const formData = {
+      access_key: 'c4768f21-4c1a-4f0b-82fc-55897303ebb8',
+
+      subject: "New Admission Enquiry - School Website",
+
+      parent_name: form.value.parentName,
+      child_name: form.value.childName,
+      class_applying: form.value.classApplying,
+      phone: form.value.phone,
+      email: form.value.email
+    };
+
+    this.http.post('https://api.web3forms.com/submit', formData)
+      .subscribe({
+        next: () => {
+          alert("Inquiry submitted successfully! We will contact you soon.");
+          form.reset();
+        },
+        error: () => {
+          alert("Something went wrong. Please try again.");
+        }
+      });
+  }
+
+  
 }
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,44 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  constructor(private http: HttpClient) {}
+
+  submitContact(form: any) {
+
+    if (form.invalid) {
+      alert("Please fill all fields");
+      return;
+    }
+
+    const formData = {
+      access_key: 'c4768f21-4c1a-4f0b-82fc-55897303ebb8',
+
+      subject: "New Contact Message - School Website",
+
+      name: form.value.firstName + " " + form.value.lastName,
+      email: form.value.email,
+      message: form.value.message,
+      user_subject: form.value.subject
+    };
+
+    this.http.post('https://api.web3forms.com/submit', formData)
+      .subscribe({
+        next: () => {
+          alert("Message sent successfully! We will contact you soon.");
+          form.reset();
+        },
+        error: () => {
+          alert("Something went wrong. Please try again later.");
+        }
+      });
+  }
 
 }
