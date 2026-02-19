4a77d65707985d4fc9ab21aa4119443a7530d2ee
Deepesh Kumar Sahu <deepesh@cdot.in>
2026-02-19 19:46:06 +0530

    updated changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'school-website';
+
+  showRouter = true;
+
+  constructor(private router: Router) {
+
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+
+        // destroy page
+        this.showRouter = false;
+
+        // recreate page
+        setTimeout(() => {
+          this.showRouter = true;
+
+          // scroll top
+          window.scrollTo(0, 0);
+
+        }, 0);
+
+      });
+  }
+
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ExtraOptions, RouterLink, RouterLinkActive, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'home', loadComponent: () => import('../../pages/home/home.component').then(m => m.HomeComponent) },
+  { path: 'about', loadComponent: () => import('../../pages/about/about.component').then(m => m.AboutComponent) },
+  { path: 'academics', loadComponent: () => import('../../pages/academics/academics.component').then(m => m.AcademicsComponent) },
+  { path: 'faculty', loadComponent: () => import('../../pages/faculty/faculty.component').then(m => m.FacultyComponent) },
+  { path: 'contact', loadComponent: () => import('../../pages/contact/contact.component').then(m => m.ContactComponent) },
+];
+
+const routerOptions: ExtraOptions = {
+  onSameUrlNavigation: 'reload',
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled'
+};
+
 
 @Component({
   selector: 'app-navbar',
